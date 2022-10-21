e3f4199479c097645907c096cd7e4ae2f68d9bfd
Hoangnh <hoangnh.xda@gmail.com>
2022-10-21 23:16:10 +0700

    Add basic smartcontract

---
+++ smartcontract/src/processor.rs
@@ -0,0 +1,19 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+pub struct Processor {}
+
+impl Processor {
+    pub fn process_instruction(
+        _program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        Ok(())
+    }
+}
